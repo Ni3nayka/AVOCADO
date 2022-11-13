7b484ecb67ff07c4f7088e7cfd152b740c4d5a3b
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-11-13 20:38:46 +0300

    Update wifi_server.py

--- src/device/wifi_server.py
+++ src/device/wifi_server.py
@@ -1,4 +1,19 @@
 import socket
+from threading import Thread
+
+class wifi_server_device_lisen(Thread):
+
+    def __init__(self,device):
+        Thread.__init__(self)
+        self.device = device 
+        self.flag = True
+
+    def run(self):
+        while self.flag:
+            print(self.device.recv(1024))
+
+    def stop(self):
+        self.flag = False
 
 class wifi_server_device:
 
@@ -13,11 +28,15 @@ class wifi_server_device:
         #print ('Got connection from', addr)
         #print (self.wifi_device.recv(1024))
 
+        self.wifi_device_potok = wifi_server_device_lisen(self.wifi_device)
+        self.wifi_device_potok.start()
+
     def send(self,data):
         self.wifi_device.send(str(data+"\n").encode('utf-8'))
 
     def close(self):
         self.device.close()
+        self.wifi_device_potok.stop()
 
 if __name__=="__main__":
     from time import sleep
