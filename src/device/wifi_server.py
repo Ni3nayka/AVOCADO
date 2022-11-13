e9602c49f1b31b9f2c9ca2f0833e43e48acb531a
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-11-14 00:45:19 +0300

    complete wifi

--- src/device/wifi_server.py
+++ src/device/wifi_server.py
@@ -7,32 +7,42 @@ class wifi_server_device_lisen(Thread):
         Thread.__init__(self)
         self.device = device 
         self.flag = True
-
-    def run(self):
-        while self.flag:
-            print(self.device.recv(1024))
+        self.line = ""
 
     def stop(self):
         self.flag = False
 
+    def run(self):
+        self.device.settimeout(1.0)
+        while self.flag:
+            try: self.line += self.device.recv(1024).decode("utf-8")
+            except socket.timeout: pass
+
+    def get(self):
+        a = self.line
+        self.line = ""
+        return a
+
 class wifi_server_device:
 
-    def __init__(self,port=12345):
+    def __init__(self,port=1234):
         self.ip = socket.gethostbyname(socket.gethostname())
         self.port = port
-        self.device = socket.socket()
+        self.device = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.device.bind((self.ip, self.port))   
         self.device.listen(5) # Now wait for client connection.
 
         self.wifi_device, addr = self.device.accept()
-        #print ('Got connection from', addr)
-        #print (self.wifi_device.recv(1024))
+        print ('Got connection from', addr)
 
         self.wifi_device_potok = wifi_server_device_lisen(self.wifi_device)
         self.wifi_device_potok.start()
 
     def send(self,data):
         self.wifi_device.send(str(data+"\n").encode('utf-8'))
+
+    def get(self):
+        return self.wifi_device_potok.get()
 
     def close(self):
         self.device.close()
@@ -43,7 +53,8 @@ if __name__=="__main__":
     test = wifi_server_device()
     for i in range(10):
         test.send(str(i))
-        #sleep(1)
+        sleep(0.1)
+    print(test.get())
     test.close()
 
 
