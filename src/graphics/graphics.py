a7428cc091cf6545d7a1d439e73903a4d8637c7a
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-11-13 20:27:28 +0300

    complete new graphics

--- src/graphics/graphics.py
+++ src/graphics/graphics.py
@@ -13,6 +13,7 @@ except ModuleNotFoundError:
     
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 from time import sleep
 from os import path
 import webbrowser
@@ -47,25 +48,29 @@ class EntryWithPlaceholder(Entry):
 
 class graphics:
     
-    def __init__(self,title='AVOCADO graphics Debug',comanda_start=0,comanda_example=0):
+    def __init__(self,title='AVOCADO graphics Debug'):
         self.name = title
         self.window = Tk()
         self.window.title(self.name)
         add_my_icon_on_window(self.window)
-        self.window.geometry('300x230')
+        #self.window.geometry('360x200')
         #self.window.resizable(0, 0)
 
         self.array_viget = []
+        self.text_monitor_viget = 0
 
-        self.input_mode = ("gamepad")#,"console")
-        self.contact_mode = ("wi-fi")#,"bluetooth","serial")
+        self.input_mode = ("геймпад","консоль","геймпад и консоль")
+        self.contact_mode = ("wi-fi","bluetooth") # ,"serial"
 
         self.input_mode_var = StringVar()
         self.contact_mode_var = StringVar()
+        self.ip_port = 1234
+        self.autoscroll_monitor_viget = True
 
         self.extern_fun = self.extern_fun_pass
 
-        self.create_main_menu()
+        #self.create_main_menu()
+        self.create_monitor_menu()
 
         # UP MENU
         self.MENU = Menu(self.window) 
@@ -77,25 +82,15 @@ class graphics:
         self.new_item_m.add_command(label='мануал по использованию программы',command=self.develop_message) 
         self.MENU.add_cascade(label='мануал', menu=self.new_item_m)
 
-
         self.new_item_i = Menu(self.MENU)  
         self.new_item_i.add_command(label='новую версию программы',command=lambda:webbrowser.open('https://github.com/Ni3nayka/AVOCADO/releases', new=2))
         self.new_item_i.add_command(label='библиотеку для esp',command=lambda:webbrowser.open('https://github.com/Ni3nayka/AVOCADO_esp/releases', new=2))
         self.MENU.add_cascade(label='скачать', menu=self.new_item_i)
 
         self.new_item_11 = Menu(self.MENU)  
-        self.new_item_11.add_command(label='оценить',command=self.develop_message) 
+        self.new_item_11.add_command(label='оценить',command=lambda:webbrowser.open('https://forms.gle/N7n5erFaajJRRawG6', new=2)) 
         self.new_item_11.add_command(label='о программе',command=self.about_program) 
         self.MENU.add_cascade(label='о программе', menu=self.new_item_11)
-
-        # self.radio_up_menu = StringVar()
-        # self.radio_up_menu.set("monitor")
-        # #self.radio_up_menu.trace("w", self.up_menu_look)
-        # self.new_item_l = Menu(self.MENU)
-        # self.new_item_l.add_radiobutton(label="консоль",value="monitor",variable=self.radio_up_menu)
-        # self.new_item_l.add_radiobutton(label="геймпад",value="gamepad",variable=self.radio_up_menu)
-        # self.new_item_l.add_radiobutton(label="геймпад и консоль",value="monitor and gamepad",variable=self.radio_up_menu)
-        # self.MENU.add_cascade(label='режим работы', menu=self.new_item_l)
 
         self.window.config(menu=self.MENU)
 
@@ -103,65 +98,132 @@ class graphics:
         messagebox.showerror("SaveSystem", "к сожалению данная опция пока в разработке")
 
     def about_program(self):
-        self.develop_message()
-        ################################################################################################################################################################
-        messagebox.showinfo("о программе", "ну типа да")
+        messagebox.showinfo("о программе", "Проект AVOCADO состоит из библиотек для разных микроконтроллеров (ESP и Arduino) и программы под windows для управления этими контроллерами с помощью библиотек при использовании минимума кода.\nАвтор: Бакай Егор\nг.Москва 2022 г.")
 
     def extern_fun_pass(self,command,data=""):
         print(command,data)
 
     def create_main_menu(self):
+        self.window.geometry('360x200')
 
         lbl_ip = Label(text="ваш IP: " + str(socket.gethostbyname(socket.gethostname())))
-        lbl_ip.place(relx=.5, rely=.5, anchor="c")
+        lbl_ip.place(relx=.5, rely=.55, anchor="c")
         self.array_viget.append(lbl_ip)
-        
-        # lbl_1 = Label(text="Подключите геймпад")
-        # lbl_1.place(relx=.5, rely=.15, anchor="c")
-        # self.array_viget.append(lbl_1)
 
-        # lbl_1 = Label(text="Выберите устройство ввода")
-        # lbl_1.place(relx=.5, rely=.05, anchor="c")
+        lbl_1 = Label(text="Выберите управляющее устройство")
+        lbl_1.place(relx=.29, rely=.15, anchor="c")
+        self.array_viget.append(lbl_1)
 
-        # input_mode_viget = ttk.Combobox(self.window, textvariable = self.input_mode_var)
-        # input_mode_viget['values'] = self.input_mode
-        # input_mode_viget['state'] = 'readonly'
-        # input_mode_viget.place(relx=.5, rely=.15, anchor="c")
+        input_mode_viget = ttk.Combobox(self.window, textvariable = self.input_mode_var)
+        input_mode_viget['values'] = self.input_mode
+        input_mode_viget['state'] = 'readonly'
+        try:
+            input_mode_viget.set(self.input_mode[list(self.input_mode).index(self.input_mode_var.get())])
+        except ValueError: pass
+        input_mode_viget.place(relx=.78, rely=.15, anchor="c")
         # def callback_1(*arg):
-        #     print(input_mode_viget.current(),self.input_mode_var.get())
+        #     #print(input_mode_viget.current(),self.input_mode_var.get())
+        #     print(self.input_mode_var.get())
         # self.input_mode_var.trace('w', callback_1)
+        self.array_viget.append(input_mode_viget)
 
-        # lbl_2 = Label(text="Выберите метод усправления")
-        # lbl_2.place(relx=.5, rely=.25, anchor="c")
+        lbl_2 = Label(text="Выберите способ передачи данных")
+        lbl_2.place(relx=.29, rely=.35, anchor="c")
+        self.array_viget.append(lbl_2)
 
-        # contact_mode_viget = ttk.Combobox(self.window, textvariable = self.contact_mode_var)
-        # contact_mode_viget['values'] = self.contact_mode
-        # contact_mode_viget['state'] = 'readonly'
-        # contact_mode_viget.place(relx=.5, rely=.35, anchor="c")
-        # def callback_2(*arg):
-        #     print(contact_mode_viget.current(),self.contact_mode_var.get())
-        # self.contact_mode_var.trace('w', callback_2)
-
-        # lbl_3 = Label(text="Запишите скорость обмена данными (Serial)")
-        # lbl_3.place(relx=.5, rely=.45, anchor="c")
+        contact_mode_viget = ttk.Combobox(self.window, textvariable = self.contact_mode_var)
+        contact_mode_viget['values'] = self.contact_mode
+        contact_mode_viget['state'] = 'readonly'
+        try:
+            contact_mode_viget.set(self.contact_mode[list(self.contact_mode).index(self.contact_mode_var.get())])
+        except ValueError: pass
+        contact_mode_viget.place(relx=.78, rely=.35, anchor="c")
+        self.array_viget.append(contact_mode_viget)
 
         # self.port_speed_viget = EntryWithPlaceholder(master=self.window,placeholder="9600")
         # self.port_speed_viget.place(relx=.5, rely=.55, anchor="c",height = 19,width = 150)
 
         lbl_4 = Label(text="Запишите номер порта")
-        lbl_4.place(relx=.4, rely=.7, anchor="c")
+        lbl_4.place(relx=.36, rely=.7, anchor="c")
         self.array_viget.append(lbl_4)
 
-        self.port_number_viget = EntryWithPlaceholder(master=self.window,placeholder="1234")
-        self.port_number_viget.place(relx=.72, rely=.7, anchor="c",height = 19,width = 50)
+        self.port_number_viget = EntryWithPlaceholder(master=self.window,placeholder=str(self.ip_port))
+        self.port_number_viget.place(relx=.7, rely=.7, anchor="c",height = 19,width = 100)
         self.array_viget.append(self.port_number_viget)
 
-        start_button = Button(text="старт",command=self.press_start) # , command=self.open_link
-        start_button.place(relx=.5, rely=.9, anchor="c",height = 30,width = 70)
+        start_button = Button(text="старт",command=self.press_start)
+        start_button.place(relx=.5, rely=.9, anchor="c",height = 30,width = 70) 
         self.array_viget.append(start_button)
 
+    def create_monitor_menu(self):
+        self.window.geometry('700x300')
+
+        # https://metanit.com/python/tkinter/2.4.php
+
+        # фрейм для нижних объектов
+        frame = Frame()
+        frame.pack(fill=X,side=BOTTOM)
+        self.array_viget.append(frame)
+
+        # кнопка выйти
+        end_button = Button(frame,text="выйти",command=self.press_stop) 
+        end_button.pack(side=LEFT, ipadx=2, ipady=2, padx=3, pady=3)
+        self.array_viget.append(end_button)
+
+        # кнопка для отправки сообщения из виджета для ввода
+        def send_wifi_message(a=0): # a - переменная нужна для работы bind при нажатии кнопки enter (просто он отправляет данные, надо как-то принять)
+            self.extern_fun("monitor_message",entry.get())
+            #self.add_line_to_text_monitor_viget(entry.get()) # test
+            entry.delete(0, END)   # удаление введенного текста
+        send_button = Button(frame,text="отправить",command=send_wifi_message) 
+        send_button.pack(side=RIGHT, ipadx=2, ipady=2, padx=3, pady=3) 
+        self.array_viget.append(send_button)
+
+        # кнопка для очистки поля вывода
+        def clear_viget():
+            self.text_monitor_viget.configure(state=NORMAL)
+            self.text_monitor_viget.delete(1.0, END) 
+            self.text_monitor_viget.configure(state=DISABLED)
+        clear_button = Button(frame,text="очистить",command=clear_viget) 
+        clear_button.pack(side=LEFT, ipadx=2, ipady=2, padx=3, pady=3) 
+        self.array_viget.append(clear_button)
+
+        # кнопка для сключения или выключения автопрокрутки
+        btn_text = StringVar(value="автопрокрутка - on")
+        self.autoscroll_monitor_viget = True
+        def update_btn_text():
+            self.autoscroll_monitor_viget = not self.autoscroll_monitor_viget
+            btn_text.set("автопрокрутка - on" if self.autoscroll_monitor_viget else "автопрокрутка - off")
+        btn_autoscroll = Button(frame,textvariable=btn_text, command=update_btn_text)
+        btn_autoscroll.pack(side=LEFT, ipadx=2, ipady=2, padx=3, pady=3) 
+        self.array_viget.append(btn_autoscroll)
+
+        # поле (снизу) для ввода пользовательского текста
+        entry = ttk.Entry(frame)
+        entry.pack(side=BOTTOM, fill=X, pady=5, ipady=2) # , padx=6, pady=6
+        entry.bind('<Return>', send_wifi_message)
+        self.array_viget.append(entry)
+
+        # большое поле для вывода полученной инфы
+        self.text_monitor_viget = Text(state='disabled')
+        scroll = Scrollbar(command=self.text_monitor_viget.yview)
+        scroll.pack(side=RIGHT, fill=Y)
+        self.text_monitor_viget.pack(side=RIGHT,fill=BOTH,padx=2, expand=True)
+        self.text_monitor_viget.config(yscrollcommand=scroll.set)
+        self.array_viget.append(self.text_monitor_viget)
+        self.array_viget.append(scroll)
+
+    def add_line_to_text_monitor_viget(self,line):
+        try: 
+            self.text_monitor_viget.configure(state=NORMAL)
+            self.text_monitor_viget.insert(END, line+"\n") 
+            if self.autoscroll_monitor_viget:
+                self.text_monitor_viget.see(END)
+            self.text_monitor_viget.configure(state=DISABLED)
+        except AttributeError: pass
+
     def create_gamepad_menu(self):
-        end_button = Button(text="стоп",command=self.press_stop) # , command=self.open_link
+        end_button = Button(text="стоп",command=self.press_stop) 
         end_button.place(relx=.5, rely=.5, anchor="c",height = 30,width = 70)
         self.array_viget.append(end_button)
 
@@ -172,17 +234,43 @@ class graphics:
     
     def press_start(self):
         try:
-            try:
-                #print(self.input_mode_var.get(),self.contact_mode_var.get(),self.port_number_viget.get())
-                port = int(self.port_number_viget.get())
-                if port<0 or port>99999:
-                    messagebox.showerror("SaveSystem", "ERROR 3: порт не может иметь такой номер")
+            # port
+            if self.contact_mode_var.get()=="wi-fi": # self.input_mode_var.get()=="геймпад":
+                try:
+                    port = int(self.port_number_viget.get())
+                except ValueError:
+                    messagebox.showerror("SaveSystem", "ERROR 2: порт не может быть не целочисленным (пример: 1234)")
                     return
-                self.extern_fun("start",port)
+                if port<0 or port>99999:
+                    messagebox.showerror("SaveSystem", "ERROR 3: порт не может иметь такой номер (пример: 1234)")
+                    return
+                self.ip_port = port
+            elif self.contact_mode_var.get()=="bluetooth":
+                port = self.port_number_viget.get()
+                if len(port.split(":"))!=6:
+                    messagebox.showerror("SaveSystem", "ERROR 6: порт не может иметь такой номер (пример: 20:A6:B6:23:0C:27)")
+                    return
+                self.ip_port = port
+            elif self.contact_mode_var.get()=="":
+                messagebox.showinfo("SaveSystem", "Выберите способ передачи данных")
+                return
+            else:
+                messagebox.showerror("SaveSystem", "ERROR 5: неизвестный режим передачи данных")
+                return
+            # graphic - device
+            commanda = "start"
+            if self.input_mode_var.get().find("геймпад")!=-1:
+                commanda += "_gamepad"
                 self.clear_viget()
                 self.create_gamepad_menu()
-            except ValueError:
-                messagebox.showerror("SaveSystem", "ERROR 2: порт не может быть не целочисленным")
+            if self.input_mode_var.get().find("консоль")!=-1:
+                commanda += "_monitor"
+                self.clear_viget()
+                self.create_monitor_menu()
+            if commanda=="start":
+                messagebox.showinfo("SaveSystem", "Выберите управляющее устройство")
+                return
+            self.extern_fun(commanda,port)
         except AttributeError:
             messagebox.showerror("SaveSystem", "ERROR 1: ошибка вызова виджета с необходимыми данными")
 
@@ -190,13 +278,6 @@ class graphics:
         self.clear_viget()
         self.create_main_menu()
         self.extern_fun("stop")
-
-        #messagebox.showerror("DevelopMessage", "Эта опция пока не реализоавна")
-
-    def open_link(self):
-        webbrowser.open('https://disk.yandex.ru/d/', new=2) # demo
-        messagebox.showerror("SaveSystem", "по такому пути нет такого файла")
-        #webbrowser.open('https://disk.yandex.ru/d/9bzTV6IxxkCIbw', new=2) # full
        
     def loop(self):
         self.window.mainloop()
--- src/test.py
+++ src/test.py
@@ -1,38 +1,36 @@
-from tkinter import *
-from tkinter import ttk
-
-# Создайте экземпляр фрейма или окна tkinter
-window = Tk()
-
-# Установить размер окна
-window.geometry("700x350")
-
-# Создайте функцию для очистки поля со списком
-def clear_combobox():
-  combobox.set('')
-
-# Определить кортеж дней
-days = ('Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье')
-
-# Функция для печати индекса выбранной опции в Combobox
-def callback(*arg):
-  Label(window, text = 'Индекс равен' + str(combobox.current()) + ' для ' + ' ' + str(var.get()), font = ('Helvetica 12') ).pack()
-
-
-# Создайте виджет со списком
-var = StringVar()
-combobox = ttk.Combobox(window, textvariable = var)
-combobox['values'] = days
-combobox['state'] = 'readonly'
-combobox.pack(fill='x',padx= 5, pady=5)
-
-# Установите трассировку для данной переменной
-var.trace('w', callback)
-
-
-# Создайте кнопку, чтобы очистить выбранное текстовое значение поля со списком
-button = Button(window, text = 'очистить', command = clear_combobox)
-button.pack()
-
-
-window.mainloop()
+from tkinter import Tk, RIGHT, BOTH, RAISED
+from tkinter.ttk import Frame, Button, Style
+ 
+ 
+class Example(Frame):
+ 
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+ 
+    def initUI(self):
+        self.master.title("Кнопки в kinter")
+        self.style = Style()
+        self.style.theme_use("default")
+ 
+        #frame = Frame(self, relief=RAISED, borderwidth=1)
+        #frame.pack(fill=BOTH, expand=True)
+ 
+        self.pack(fill=BOTH, expand=True)
+ 
+        closeButton = Button(text="Закрыть")
+        closeButton.pack(side=RIGHT, padx=5, pady=5)
+        okButton = Button(text="Готово")
+        okButton.pack(side=RIGHT)
+ 
+ 
+def main():
+ 
+    root = Tk()
+    root.geometry("300x200+300+300")
+    app = Example()
+    root.mainloop()
+ 
+ 
+if __name__ == '__main__':
+    main()
