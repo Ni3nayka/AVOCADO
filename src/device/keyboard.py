0d191b4bb3c3c2b946b5805e02aa5904278fd4cc
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2023-05-21 12:19:56 +0300

    Update keyboard.py

--- src/device/keyboard.py
+++ src/device/keyboard.py
@@ -8,22 +8,41 @@ modify: May 2023
 '''
 import tkinter
 
-root = tkinter.Tk()
+class my_keyboard:
 
-def get_key_data(event):
-    char = event.char
-    if char=='': char = event.keysym
-    code = event.keycode
-    print(char,code)
-    #print(event.char,event.keysym,event.keycode,type(event.char))
-def key_handler(event):
-    print(end='+ ')
-    get_key_data(event)
-def key_handler_off(event):
-    print(end='- ')
-    get_key_data(event)
+    def __init__(self,extern_fun=None,window=None):
+        self.window = window
+        if self.window==None:
+            self.window = tkinter.Tk()
+        self.extern_fun = extern_fun
+        if self.extern_fun==None:
+            self.extern_fun = self._extern_fun_pass
 
-root.bind("<KeyPress>", key_handler)
-root.bind("<KeyRelease>", key_handler_off)
+        # def get_key_data(event):
+        #     char = event.char
+        #     if char=='': char = event.keysym
+        #     code = event.keycode
+        #     print(char,code)
+        #     print(event.char,event.keysym,event.keycode,type(event.char))
+        def key_handler(event):
+            self.extern_fun("+"+str(event.keycode))
+        def key_handler_off(event):
+            self.extern_fun("-"+str(event.keycode))
 
-root.mainloop()
+        self.window.bind("<KeyPress>", key_handler)
+        self.window.bind("<KeyRelease>", key_handler_off)
+
+        if window==None:
+            self.window.mainloop()
+
+    def destroy(self):
+        self.window.unbind("<KeyPress>")
+        self.window.unbind("<KeyRelease>")
+        
+    def _extern_fun_pass(self,data):
+        print(data)
+
+if __name__ == "__main__":
+    def test(a):
+        print("qwerty:",a)
+    keyboard = my_keyboard(test)
