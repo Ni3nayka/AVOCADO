28e5cfd3858b9631fd2e09c579e74b846fb78f68
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-01-11 21:41:51 +0300

    update linux install

--- install.sh
+++
@@ -1,8 +0,0 @@
-# python3 -m Tkinter
-sudo apt-get install python3-tk
-sudo apt install python3-pip
-pip3 install pillow
-sudo apt-get install python3-pil python3-pil.imagetk
-pip3 install pygame
-python3 main.py
-
---
+++ linux_install.sh
@@ -0,0 +1,8 @@
+# python3 -m Tkinter
+sudo apt-get install python3-tk -y
+sudo apt install python3-pip -y
+pip3 install pillow
+sudo apt-get install python3-pil python3-pil.imagetk -y
+pip3 install pygame
+# python3 main.py
+
