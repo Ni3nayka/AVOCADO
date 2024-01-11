4dd6084ff64b4c85ce6ae8217bc9a15f36fca9af
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-01-11 22:20:01 +0300

    update install linux

--- install.sh
+++ install.sh
@@ -1,8 +1,26 @@
 # python3 -m Tkinter
-sudo apt-get install python3-tk
-sudo apt install python3-pip
+sudo apt-get install python3-tk -y
+sudo apt install python3-pip -y
 pip3 install pillow
-sudo apt-get install python3-pil python3-pil.imagetk
+sudo apt-get install python3-pil python3-pil.imagetk -y
 pip3 install pygame
-python3 main.py
+# python3 main.py
+echo "[Desktop Entry]" > avocado_linux_run.desktop
+echo "Version=1.0" >> avocado_linux_run.desktop
+echo "Type=Application" >> avocado_linux_run.desktop
+echo "Terminal=true" >> avocado_linux_run.desktop
+echo "Icon=mate-panel-launcher" >> avocado_linux_run.desktop
+echo "Icon[ru]=mate-panel-launcher" >> avocado_linux_run.desktop
+echo "Name[ru]=avocado_linux_run" >> avocado_linux_run.desktop
+echo "Exec=python3 $( dirname -- "$( readlink -f -- "$0"; )"; )/src/main.py" >> avocado_linux_run.desktop # echo "dirname/readlink: $( dirname -- "$( readlink -f -- "$0"; )"; )"
+echo "Name=test" >> avocado_linux_run.desktop
+#gio set avocado_linux_run.desktop "metadata::trusted" yes
+sudo chmod +x avocado_linux_run.desktop
+
+
+
+
+
+
+
 
