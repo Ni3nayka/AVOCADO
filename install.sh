459cfa3036d0d44eb9e454a7531875b2ae52f79c
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-02-19 11:35:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,55 @@
-# python3 -m Tkinter
-sudo apt-get install python3-tk -y
-sudo apt install python3-pip -y
-pip3 install pillow
-sudo apt-get install python3-pil python3-pil.imagetk -y
-pip3 install pygame
-# python3 main.py
-echo "[Desktop Entry]" > avocado_linux_run.desktop
-echo "Version=1.0" >> avocado_linux_run.desktop
-echo "Type=Application" >> avocado_linux_run.desktop
-echo "Terminal=true" >> avocado_linux_run.desktop
-echo "Icon=mate-panel-launcher" >> avocado_linux_run.desktop
-echo "Icon[ru]=mate-panel-launcher" >> avocado_linux_run.desktop
-echo "Name[ru]=avocado_linux_run" >> avocado_linux_run.desktop
-echo "Exec=python3 $( dirname -- "$( readlink -f -- "$0"; )"; )/src/main.py" >> avocado_linux_run.desktop # echo "dirname/readlink: $( dirname -- "$( readlink -f -- "$0"; )"; )"
-echo "Name=test" >> avocado_linux_run.desktop
-#gio set avocado_linux_run.desktop "metadata::trusted" yes
-sudo chmod +x avocado_linux_run.desktop
+#!/bin/bash
 
+# links:
+# no output: https://stackoverflow.com/questions/18062778/how-to-hide-command-output-in-bash
+# python to sh: https://trstringer.com/python-in-shell-script/
+# goto: https://stackoverflow.com/questions/9639103/is-there-a-goto-statement-in-bash
+# if in one line: https://andreyex.ru/linux/kak-proverit-uspeshnost-vypolneniya-komandy-v-bash/
+
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [ $? -eq 0 ]; then
+    echo "Установка необходимых зависимостей..."
+    # python3 -m Tkinter
+    {
+    sudo apt update
+    sudo apt install python3-tk -y
+    sudo apt install python3-pip -y
+    pip3 install pillow
+    sudo apt install python3-pil python3-pil.imagetk -y
+    pip3 install pygame
+    } &> /dev/null
+    echo "Необходимые зависимости установлены"
+else
+    echo "ОШИБКА: Отсутствует подключение к интернету, необходимые зависимости НЕ БУДУТ установлены!!!"
+fi
+
+echo "Создаем файл-ярлык, для запуска программы..."
+{
+PYCMD=$(cat <<EOF
+file = open("avocado_linux_run.desktop",'w')
+file.write("[Desktop Entry]\n")
+file.write("Version=1.0\n")
+file.write("Type=Application\n")
+file.write("Terminal=true\n")
+file.write("Icon=mate-panel-launcher\n")
+file.write("Icon[ru]=mate-panel-launcher\n")
+file.write("Name[ru]=avocado_linux_run\n")
+def test_line(a):
+    if " " in a: a = '\'' + a + '\''
+    return a
+import os
+path = os.path.abspath("src/main.py")
+path = '/'.join(map(test_line, path.split("/")))
+file.write(f"Exec=python3 {path}\n")
+file.write("Name=test\n")
+file.close()
+
+EOF
+)
+python3 -c "$PYCMD"
+sudo chmod +x avocado_linux_run.desktop
+} &> /dev/null
+echo "Файл-ярлык создан, конец"
 
 
 
